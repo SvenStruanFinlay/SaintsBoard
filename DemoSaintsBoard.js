9b983f5378c9b9e888795b96965054797ad32785
oplde <oplde@pc5-029-l.cs.st-andrews.ac.uk>
2016-10-08 18:41:15 +0100

    implemented demo js

--- DemoSaintsBoard.js
+++ DemoSaintsBoard.js
@@ -1,12 +1,38 @@
-var site="linttosite";
+var site="linktosite";
 var app = angular.module('board' , []);
 app.controller('SaintBoardCtrl',function($scope,$http){
   $scope.posts=[];
+  $scope.demoServerPosts=[];
+  $scope.currId=1;
   $scope.submitPost= function(postText){
     //send data to server code
+    demoServerPosts.push(
+  {
+    id:$scope.currId++,
+    date:moment.now(),
+    post: postText
+    comments:[
+      // {
+      //   date:moment.now()
+      //   commentText:"text"
+      // },
+      // {
+      //   date:moment.now()
+      //   commentText:"text2"
+      // }
+    ]});
+
     $.post('localhost:8081',{type : "post", post : postText});
   }
-  $scope.submitComment= function(id, commentText){
+  $scope.submitComment= function(postID, commentText){
+    currpost;
+    for (var i = 0; i < demoServerPosts.length; i++) {
+      if (demoServerPosts[i].id == postID) {
+        currpost=demoServerPosts[i];
+      }
+    }
+    currpost.comments.push({date:moment.now(),commentText:commentText});
+
     $.post('localhost:8081',{type : "comment", comment : postText, postId : id});
   }
   $scope.getPostsRating= function(){
@@ -28,6 +54,7 @@ app.controller('SaintBoardCtrl',function($scope,$http){
         alerts.alert("alerts","error getting the posts, server returned: " +response.status);
       }
     )
+    posts=demoServerPosts;
   }
   $scope.formatDate= function(date){
     return moment(date).format('MMMM Do YYYY [at] h:mm:ss a');
