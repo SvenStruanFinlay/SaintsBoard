0e66821823071980ef75e62f197c6688645ea477
oplde <oplde@st-andrews.ac.uk>
2016-10-08 16:16:59 +0100

    Update SaintBoard.js
    added semicolons and error to get request

--- SaintBoard.js
+++ SaintBoard.js
@@ -4,10 +4,10 @@ app.controller('SaintBoardCtrl',function($scope,$http){
   $scope.posts=[];
   $scope.submitPost= function(postText){
     //send data to server code
-    $.post('localhost:8081',{type : "post", post : postText})
+    $.post('localhost:8081',{type : "post", post : postText});
   }
   $scope.submitComment= function(id, commentText){
-    $.post('localhost:8081',{type : "comment", comment : postText, postId : id})
+    $.post('localhost:8081',{type : "comment", comment : postText, postId : id});
   }
   $scope.getPostsRating= function(){
     getData(site+"?rating");
@@ -24,7 +24,9 @@ app.controller('SaintBoardCtrl',function($scope,$http){
       function success(response){
 
       }
-      //function console.error();
+      function error(response){
+        alerts.alert("alerts","error getting the posts, server returned: " +response.status);
+      }
     )
   }
   $scope.formatDate= function(date){
