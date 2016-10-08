ac769c9baec5eedb14b1a869c9000d10496b32c6
Chris <cr217@st-andrews.ac.uk>
2016-10-08 16:39:24 +0100

    server.js Update: Server now recieves POST requestsgit add .git add .

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
-http = require('http');
-fs = require('fs');
-server = http.createServer( function(req, res) {
-  console.log("asdasdads");
-  console.dir(req.param);
+var express = require('express');
+var fs = require('fs');
+var bodyParser=require('body-parser')
+var app = express();
+var bodyparser= bodyParser();
 
-  if (req.method == 'POST') {
-    console.log("POST");
-    var body = '';
-    req.on('data', function (data) {
-      body += data;
-      console.log("Partial body: " + body);
-    });
-    req.on('end', function () {
-      console.log("Body: " + body);
-    });
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('post received');
-  }
-  else
-  {
-    console.log("GET");
-    //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-    var html = fs.readFileSync('index.html');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-  }
+app.use(bodyparser);
 
+app.get('/', function(req, res){
+  console.log('GET /')
+  //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+  var html = fs.readFileSync('index.html');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(html);
+});
+
+app.post('/', function(req, res){
+  console.log('POST /');
+  console.dir(req.body);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('thanks');
 });
 
 port = 8081;
-//138.251.246.252
-host = 'localhost';
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
+app.listen(port);
+console.log('Listening at http://localhost:' + port)
