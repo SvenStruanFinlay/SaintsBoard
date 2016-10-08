07b1786f133860ecffc2d48a76848a939641b252
oplde <oplde@pc5-029-l.cs.st-andrews.ac.uk>
2016-10-08 15:30:30 +0100

    linking between frontend and backent a bit

--- SaintBoard.js
+++ SaintBoard.js
@@ -1,19 +1,31 @@
-
+var site="linttosite";
 var app = angular.module('board' , []);
 app.controller('SaintBoardCtrl',function($scope,$http){
   $scope.posts=[];
   $scope.submitPost= function(postText){
     //send data to server code
+    $.post('linkToSite',{type : "post", post : postText})
   }
   $scope.submitComment= function(id, commentText){
-
+    $.post('linkToSite',{type : "comment", comment : postText, postId : id})
   }
   $scope.getPostsRating= function(){
+    getData(site+"?rating");
     //ask server for data
     //populatelist(through ng-repeat)
   }
   $scope.getPostsDate= function(){
+    getData(site+"?date");
 
+  }
+  getData=function(requestsite){
+
+    $http.get("linkToSite?rating").then(
+      function success(response){
+
+      }
+      function console.error();
+    )
   }
   $scope.formatDate= function(date){
     return moment(date).format('MMMM Do YYYY [at] h:mm:ss a');
