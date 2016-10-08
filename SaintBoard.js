fc89b1cc1413786695f7ed2bc61b3366c8103729
oplde <oplde@pc5-029-l.cs.st-andrews.ac.uk>
2016-10-08 13:59:41 +0100

    worked on html, and angular implementation

--- SaintBoard.js
+++ SaintBoard.js
@@ -1,1 +1,21 @@
 
+var app = angular.module('board' , []);
+app.controller('SaintBoardCtrl',function($scope,$http){
+  $scope.posts;
+  $scope.submitPost = function(){
+    //send data to server code
+  }
+  $scope.getPostsRating(){
+    //ask server for data
+    //populatelist(through ng-repeat)
+  }
+  $scope.getPostsDate(){
+
+  }
+  $scope.formatDate(date){
+    return moment(date).format('MMMM Do YYYY [at] h:mm:ss a');
+  }
+});
+$(document).ready(function(){
+  angular.bootstrap(document, ['board']);
+});
