1842ea715ce9d7d3a54dca5f2cd1d61079755754
oplde <oplde@pc5-029-l.cs.st-andrews.ac.uk>
2016-10-08 14:47:01 +0100

    frontend only functionality finished?

--- SaintBoard.js
+++ SaintBoard.js
@@ -1,19 +1,25 @@
 
 var app = angular.module('board' , []);
 app.controller('SaintBoardCtrl',function($scope,$http){
-  $scope.posts;
-  $scope.submitPost = function(){
+  $scope.posts=[];
+  $scope.submitPost= function(postText){
     //send data to server code
   }
-  $scope.getPostsRating(){
+  $scope.submitComment= function(id, commentText){
+
+  }
+  $scope.getPostsRating= function(){
     //ask server for data
     //populatelist(through ng-repeat)
   }
-  $scope.getPostsDate(){
+  $scope.getPostsDate= function(){
 
   }
-  $scope.formatDate(date){
+  $scope.formatDate= function(date){
     return moment(date).format('MMMM Do YYYY [at] h:mm:ss a');
+  }
+  $scope.toggleComment= function(topID){
+    $("#topID #comments").toggle();
   }
 });
 $(document).ready(function(){
